22096d4250b851b59e7fea14895c03dcb4faac94
Kavi-2611 <kavyabrahmbhatt@gmail.com>
2024-03-27 18:00:07 -0400

    Changes of the backend for mens womens and kids

--- backend/controllers/user/indexController.js
+++ backend/controllers/user/indexController.js
@@ -21,7 +21,44 @@ const getProduct = async (req, res) => {
   }
 };
 
+const getProductByCategory = async (req, res) => {
+  const categoryName = req.params.category;
+
+  try {
+    const category = await Category.findOne({ name: categoryName });
+
+    if (!category) {
+      return res.status(404).json({ message: 'Category not found' });
+    }
+
+    const products = await Product.find({ category: category._id }).populate('category');
+
+    res.json(products);
+  } catch (error) {
+    console.error('Error fetching products by category:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+const getProductById = async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    res.json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
+};
+
 module.exports = {
   getCategory,
   getProduct,
+  getProductByCategory,
+  getProductById,
 };
